84bc1dcb884d078d9a58832c7d471cf7d1cc67c1
somia13 <107176119+somia13@users.noreply.github.com>
2023-01-10 17:00:12 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import SignUp from "./Components/SignUp";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-
-
-    
-<Routes>
-
-<Route path='/' element={<SignUp/>} />
-  
-</Routes>
-
-    </>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
